5761e4a6eb52e71373220bb0c23abfba64e59b8d
Marc-André Faucher <ma.faucher@gmail.com>
2014-01-30 15:38:31 -0500

    Restructuring

--- src/clac/creole/scope/AdjScope.java
+++ src/clac/creole/scope/AdjScope.java
@@ -38,43 +38,35 @@ public class AdjScope extends AbstractLanguageAnalyser
 
     protected String inputAnnotationSetName;
     protected String outputAnnotationSetName;
+    private AnnotationSet inAnns;
+    private AnnotationSet outAnns;
     protected String triggerAnnName;
     protected String scopeAnnName;
 
     public void execute() throws ExecutionException {
+        inAnns = document.getAnnotations(inputAnnotationSetName);
+        outAnns = document.getAnnotations(outputAnnotationSetName);
         if (document == null) {
             throw new GateRuntimeException("No document to process!");
         }
-        AnnotationSet inAnns  = document.getAnnotations(inputAnnotationSetName);
-        AnnotationSet outAnns = document.getAnnotations(outputAnnotationSetName);
         AnnotationSet triggers = inAnns.get(triggerAnnName);
         for (Annotation trigger : triggers) {
-            // Make sure no scope exists for this trigger
-            Annotation scope = getScope(trigger, inAnns);
-            if (scope != null) {
-                    System.err.println("Warning: existing scope for trigger '"
-                                      +getAnnotationText(trigger)+"' -> '"
-                                      +getAnnotationText(scope
-            }
             // Triggers should overlap with a token
             Annotation token = getCoextensive(trigger, inAnns.get("Token"));
             if (token == null) {
                 System.err.println("Warning: no token for trigger: "
                                   +getAnnotationText(trigger));
-                return;
+                continue;
             }
-            // Ignore non-adjectives TODO: What about nominal modifiers?
-            if (!filterPos(token, "JJ")) continue;
-            // Heuristics
-            modScope(trigger, inAnns, outAnns);
+            adjScope(trigger);
         }
     }
 
     /** Annotate the scope of a given trigger */
-    private void modScope(Annotation trigger, AnnotationSet inAnns, AnnotationSet outAnns) {
+    private void adjScope(Annotation trigger) {
         Annotation token = getCoextensive(trigger, inAnns.get("Token"));
-        // Triggers should only have one scope, issue a warning otherwise
-        boolean scopeFound = false;
+        // Ignore non-adjectives TODO: What about nominal modifiers?
+        if (!filterPos(token, "JJ")) return;
         // Iterate through all Dependencies TODO: Inefficient, limit by offset
         for (Annotation dep : inAnns.get("Dependency")) {
             String kind = dep.getFeatures().get("kind").toString().trim();
@@ -83,33 +75,68 @@ public class AdjScope extends AbstractLanguageAnalyser
             String[] args = ids.split("\\,");
             int depId = Integer.parseInt(args[1].trim());
             int govId = Integer.parseInt(args[0].trim());
+
             // Check *mod dependencies
             for (int i = 0; i < modDepList.length; i++) {
                 if (kind.equals(modDepList[i]) && token.getId() == depId) {
-                    if (scopeFound) {
-                        System.err.println( "Warning: Multiple '*mod' dependencies for trigger: "
-                                          + getAnnotationText(token) + " (" + modDepList[i] + ")" );
-                        continue;
-                    }
-                    try {
-                        Annotation scope = (Annotation) inAnns.get(govId);
-                        Long start = scope.getStartNode().getOffset();
-                        Long end = scope.getEndNode().getOffset();
-                        FeatureMap fm = gate.Factory.newFeatureMap();
-                        fm.put("heuristic", dep.getFeatures().get("kind").toString().trim());
-                        fm.put("triggerID", trigger.getId());
-                        fm.put("tokenID",   token.getId());
-                        outAnns.add(start, end, scopeAnnName, fm);
-                        scopeFound = true;
-                    } catch (InvalidOffsetException e) {
-                        e.printStackTrace();
-                    }
+                    Annotation scope = (Annotation) inAnns.get(govId);
+                    String heuristic = dep.getFeatures().get("kind").toString();
+                    annotateScope(scope, trigger, heuristic);
                 }
             }
         }
     }
 
+    /** Wrapper which uses single annotation as the scope */
+    private void annotateScope(Annotation scope, Annotation trigger, String heuristic) {
+        try {
+            Long startOffset = scope.getStartNode().getOffset();
+            Long endOffset   = scope.getEndNode().getOffset();
+            annotateScope(startOffset, endOffset, trigger, heuristic);
+        } catch (InvalidOffsetException e) {
+            System.out.println("Error: invalid scope offsets.");
+            e.printStackTrace();
+        }
+    }
+    /** Standard function for creating scope annotation and features */
+    private void annotateScope(Long startOffset, Long endOffset,
+            Annotation trigger, String heuristic)
+            throws InvalidOffsetException {
+        // If scope already exists issue a warning
+        Annotation scope = getScope(trigger);
+        if (scope != null) {
+            String newScope = this.getDocument().getContent().getContent(
+                    startOffset, endOffset).toString();
+            System.err.println("Warning: Multiple scopes detected for trigger:");
+            System.err.println("OLD: "+getAnnotationText(trigger)+"' -> ("
+                              +scope.getFeatures().get("heuristic")+") "
+                              +getAnnotationText(scope));
+            System.err.println("NEW: "+getAnnotationText(trigger)+"' -> ("
+                              +heuristic+") "+newScope);
+        // Otherwise annotate scope
+        } else {
+            FeatureMap fm = gate.Factory.newFeatureMap();
+            fm.put("heuristic", heuristic);
+            fm.put("triggerID", trigger.getId());
+            outAnns.add(startOffset, endOffset, scopeAnnName, fm);
+        }
+    }
+
+    /** Returns true iff token's POS matches given POS. */
+    private boolean filterPos(Annotation token, String pos) {
+        return filterPos(token, pos, false);
+    }
+    private boolean filterPos(Annotation token, String pos, boolean strict) {
+        String tokenPos = token.getFeatures().get("category").toString();
+        if (strict) return tokenPos.equals(pos);
+        return ( tokenPos.length() >= pos.length() &&
+                 tokenPos.substring(0, pos.length()).equals(pos) );
+    }
+
     /** Find the scope which corresponds to this trigger or token */
+    private Annotation getScope(Annotation trigger) {
+        return getScope(trigger, inAnns);
+    }
     private Annotation getScope(Annotation trigger, AnnotationSet alist) {
         AnnotationSet scopes = alist.get(scopeAnnName);
         for (Annotation scope : scopes) {
@@ -121,17 +148,6 @@ public class AdjScope extends AbstractLanguageAnalyser
             }
         }
         return null;
-    }
-
-    /** Returns true iff token's POS matches given POS. */
-    private boolean filterPos(Annotation token, String pos) {
-        filterPos(token, pos, false);
-    }
-    private boolean filterPos(Annotation token, String pos, boolean strict) {
-        String tokenPos = token.getFeatures().get("category");
-        if (strict) return tokenPos.equals(pos);
-        return ( tokenPos.length() >= pos.length() &&
-                 tokenPos.substring(0, pos.length()).equals(pos) );
     }
 
     /** Find the first coextensive annotation in a list or return null */
@@ -192,6 +208,7 @@ public class AdjScope extends AbstractLanguageAnalyser
     @CreoleParameter(comment = "The annotation set name for the input annotations")
     public void setInputAnnotationSetName(String inputAnnotationSetName) {
         this.inputAnnotationSetName = inputAnnotationSetName;
+        this.inAnns = document.getAnnotations(inputAnnotationSetName);
     }
 
     public String getInputAnnotationSetName() {
@@ -203,6 +220,7 @@ public class AdjScope extends AbstractLanguageAnalyser
     @CreoleParameter(comment = "The annotation set name for the output annotations")
     public void setOutputAnnotationSetName(String outputAnnotationSetName) {
         this.outputAnnotationSetName = outputAnnotationSetName;
+        this.outAnns = document.getAnnotations(outputAnnotationSetName);
     }
 
     public String getOutputAnnotationSetName() {
